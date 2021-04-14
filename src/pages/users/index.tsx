1d2b1d71799c7f5b5c4141f84caa963d8b6f1cd8
Italo Almeida <italoalmeida89@gmail.com>
2021-04-14 10:50:44 -0300

    create user form page

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -11,7 +11,6 @@ export default function UserList() {
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
 
-
         <Box flex="1" borderRadius={8} bg="gray.800" p="8" >
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">Usuários</Heading>
