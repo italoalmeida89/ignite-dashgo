14545b369946255539ea88a5e7d6861379f537e3
Italo Almeida <italoalmeida89@gmail.com>
2021-04-14 12:42:56 -0300

    refactor including link from next

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,17 +1,19 @@
-import { Text, Link, Icon, LinkProps as ChakraLinkProps } from "@chakra-ui/react";
+import { Text, Link as ChakraLink, Icon, LinkProps as ChakraLinkProps } from "@chakra-ui/react";
 import { ElementType } from "react";
-import { RiContactsLine } from "react-icons/ri";
-
+import Link from 'next/link'
 interface NavLinkProps extends ChakraLinkProps {
-  icon: ElementType
-  children: string
+  icon: ElementType;
+  children: string;
+  href: string;
 }
 
-export function NavLink({ icon, children, ...rest }: NavLinkProps) {
+export function NavLink({ icon, children, href, ...rest }: NavLinkProps) {
   return (
-    <Link display="flex" align="center" {...rest}>
-      <Icon as={icon} fontSize="20" />
-      <Text ml="4" fontWwight="medium">{children}</Text>
-    </Link >
+    <Link href={href} passHref>
+      <ChakraLink display="flex" align="center" {...rest}>
+        <Icon as={icon} fontSize="20" />
+        <Text ml="4" fontWwight="medium">{children}</Text>
+      </ChakraLink >
+    </Link>
   );
 }
--- src/pages/users/create.tsx
+++ src/pages/users/create.tsx
@@ -2,6 +2,7 @@ import { Box, Flex, Heading, Divider, VStack, SimpleGrid, HStack, Button } from 
 import { Input } from '../../components/Form/Input'
 import { Header } from "../../components/Header";
 import { Sidebar } from "../../components/Sidebar";
+import Link from 'next/link'
 
 export default function CreateUser() {
   return (
@@ -29,7 +30,9 @@ export default function CreateUser() {
 
           <Flex mt="8" justify="flex-end">
             <HStack spacing="4">
-              <Button colorScheme="whiteAlpha">Cancelar</Button>
+              <Link href="/users" passHref>
+                <Button as="a" colorScheme="whiteAlpha">Cancelar</Button>
+              </Link>
               <Button colorScheme="pink">Salvar</Button>
 
             </HStack>
